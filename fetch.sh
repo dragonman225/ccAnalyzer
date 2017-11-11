9c2c444c3b2dd8cea5113579232d59c16b471f8f
dragonman225 <zxcvb22217@gmail.com>
2017-11-11 22:57:45 +0800

    update for change in coinmarketcap api

--- fetch.sh
+++ fetch.sh
@@ -2,4 +2,4 @@
 
 DATE=$(date --rfc-3339=seconds | sed 's/ /T/')
 FILENAME=/home/alexander/Documents/CryptoCatch/raw/rawData_$DATE.json
-wget https://api.coinmarketcap.com/v1/ticker/ -O $FILENAME
+wget https://api.coinmarketcap.com/v1/ticker/?limit=10000 -O $FILENAME
